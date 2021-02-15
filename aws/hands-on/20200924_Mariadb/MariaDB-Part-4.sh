45b2ff735caddcb45ebf2a3a3cd52b22e732ac0a
Necip <necipsaylan@gmail.com>
2021-02-15 23:15:41 +0300

    20210215

--- aws/hands-on/20200924_Mariadb/MariaDB-Part-4.sh
+++ aws/hands-on/20200924_Mariadb/MariaDB-Part-4.sh
@@ -17,7 +17,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install mariadb-client
 
 # Connect the clarusdb on MariaDB Server on the other EC2 instance (pw:clarus1234).
-mysql -u clarususer -p -h ec2-54-144-53-247.compute-1.amazonaws.com
+mysql -h ec2-54-144-53-247.compute-1.amazonaws.com -u clarususer -p  #güncellenecek
 
 # Show that clarususer can do same db operations on MariaDB Server instance.
 SHOW DATABASES;
