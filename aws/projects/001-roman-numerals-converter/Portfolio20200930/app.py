629e296ac2a2a0aab58d742982571cb876db567f
Necip <necipsaylan@gmail.com>
2021-02-21 23:15:52 +0300

    20210219

--- aws/projects/001-roman-numerals-converter/Portfolio20200930/app.py
+++ aws/projects/001-roman-numerals-converter/Portfolio20200930/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
+import cevir
 
 app = Flask(__name__)
 
-def convert(decimal_num):
-    roman= {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
-    num_to_roman=''
-    for i in roman.keys():
-        num_to_roman += roman[i]*(decimal_num//i)
-        decimal_num %=i
-    return num_to_roman
+#def convert(decimal_num):
+#    roman= {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+#    num_to_roman=''
+#    for i in roman.keys():
+#        num_to_roman += roman[i]*(decimal_num//i)
+#        decimal_num %=i
+#    return num_to_roman
 
 @app.route('/', methods=['GET'])
 def main_get():
@@ -24,9 +25,9 @@ def main_post():
     if not 0 < number < 4000:
         return render_template('index.html', developer_name='Necip Saylan', not_valid=True)
 
-    return render_template('result.html', number_decimal= number, number_roman= convert(number) , developer_name='Necip Saylan')
+    return render_template('result.html', number_decimal= number, number_roman= cevir.convert(number) , developer_name='Necip Saylan')
 
 
 if __name__=='__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
